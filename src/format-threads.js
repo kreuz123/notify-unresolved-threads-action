193262f59b21d8d58754769894cf77b59d547b69
CCHuang <125183578+kreuz123@users.noreply.github.com>
2026-08-10 14:48:44 +0300

    Add formatThreadList function for thread previews

---
+++ src/format-threads.js
@@ -0,0 +1,15 @@
+const PREVIEW_LENGTH = 70;
+
+function formatThreadList(threads) {
+  return threads
+    .map((thread, index) => {
+      const preview = thread.comments.nodes[0].body.substring(0, PREVIEW_LENGTH);
+      const isPreviewCut = preview.length >= PREVIEW_LENGTH;
+      const ellipsis = isPreviewCut ? '...' : '';
+      
+      return `${index + 1}. [View thread](${thread.comments.nodes[0].url}) - ${preview}${ellipsis}`;
+    })
+    .join('\n');
+}
+
+module.exports = { formatThreadList };
