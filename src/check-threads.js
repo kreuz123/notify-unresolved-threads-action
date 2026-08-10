13a65d7eec99236efedaf397c065ecec96f18ed4
CCHuang <125183578+kreuz123@users.noreply.github.com>
2026-08-10 14:47:39 +0300

    Add function to check unresolved review threads

---
+++ src/check-threads.js
@@ -0,0 +1,61 @@
+async function checkUnresolvedThreads(client, owner, repo, prNumber, reviewer) {
+  const allThreads = [];
+  let hasNextPage = true;
+  let endCursor = null;
+
+  while (hasNextPage) {
+    const { repository } = await client.graphql(
+      `
+        query($owner: String!, $repo: String!, $number: Int!, $after: String) {
+          repository(owner: $owner, name: $repo) {
+            pullRequest(number: $number) {
+              reviewThreads(first: 100, after: $after) {
+                nodes {
+                  isResolved
+                  comments(first: 1) {
+                    nodes {
+                      author {
+                        login
+                      }
+                      body
+                      url
+                    }
+                  }
+                }
+                pageInfo {
+                  hasNextPage
+                  endCursor
+                }
+              }
+            }
+          }
+        }
+      `,
+      {
+        owner,
+        repo,
+        number: prNumber,
+        after: endCursor
+      }
+    );
+
+    const threads = repository.pullRequest.reviewThreads.nodes;
+    allThreads.push(...threads);
+    
+    hasNextPage = repository.pullRequest.reviewThreads.pageInfo.hasNextPage;
+    endCursor = repository.pullRequest.reviewThreads.pageInfo.endCursor;
+  }
+
+  // Filter: unresolved threads started by this reviewer
+  const unresolvedThreads = allThreads.filter(thread => {
+    return (
+      !thread.isResolved &&
+      thread.comments.nodes.length > 0 &&
+      thread.comments.nodes[0]?.author?.login === reviewer
+    );
+  });
+
+  return unresolvedThreads;
+}
+
+module.exports = { checkUnresolvedThreads };
